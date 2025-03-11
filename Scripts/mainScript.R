6856c31a8575a1ba40d57fd188dbbfc833fc0a75
hiramRV <stevenrv97@gmail.com>
2025-03-11 09:04:50 +0100

    New Graph
    First script to generate a barplot for grade distribution.

--- Scripts/mainScript.R
+++ Scripts/mainScript.R
@@ -1,4 +1,20 @@
 ###
 # Base script to generate graphs
+# Contributors:
+# 
 ###
+
+# A Basic data frame 
+notes.data = data.frame(
+  grade= c("A","B","C","D","E","F"), 
+  count = c(17.5,16.5,10.7,6.8,12.6,35.9)
+)
+# print the data frame
+notes.data$count = notes.data$count/100.0 
+print(notes.data)
+
+# Create Bar Plot
+barplot(height = notes.data$count,names.arg = notes.data$grade,
+        main="Grades distribution", ylab="Percentage")
+
 
